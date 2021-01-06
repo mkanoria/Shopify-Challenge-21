ab7d57301e3d4754c2e88ec4cdce5448f05f0fa7
Mayank Kanoria <mayankkanoria@gmail.com>
2021-01-06 12:11:25 -0500

    Setup firebase

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
+// import express from "express";
+// import multer from "multer";
 const express = require("express");
-const app = express(); // initialize express instance
+const multer = require("multer");
+const firebase = require("firebase");
 
-app.get("/", (req, res) => {
+// initialize express instance
+const app = express();
+const upload = multer({ dest: "uploads/" });
+
+// Get firebase config info
+const firebaseConfig = require("./config/firebase").firebaseConfig;
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+app.post("/upload", upload.single("picture"), (req, res) => {
+  console.log(req.file);
+  res.send("Uploading file!");
+});
+
+app.get("/", async (req, res) => {
   res.send("Hello World");
+  const docRef = db.collection("users").doc("alovelace");
+
+  await docRef.set({
+    first: "Ada1",
+    last: "Lovelace",
+    born: 1815,
+  });
 });
 
 // Server running on port 5000
-app.listen(5000);
+app.listen(5000, () => {
+  console.log("Listening on 5000");
+});
