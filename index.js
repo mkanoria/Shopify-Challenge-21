3571f06b1f0d858890d5e197d5986f6dc3b4bb32
Mayank Kanoria <mayankkanoria@gmail.com>
2021-01-08 15:26:52 -0500

    Image upload works

--- index.js
+++ index.js
@@ -1,33 +1,86 @@
-// import express from "express";
-// import multer from "multer";
+/*
 const express = require("express");
-const multer = require("multer");
-const firebase = require("firebase");
+const bodyParser = require("body-parser");
+// Cloudinary Setup
+var cloudinary = require("cloudinary");
 
 // initialize express instance
 const app = express();
-const upload = multer({ dest: "uploads/" });
 
+
+app.get("/", async (req, res) => {
+  res.send("Hello World");
+  // const docRef = db.collection("users").doc("alovelace");
+
+  // await docRef.set({
+  //   first: "Ada1",
+  //   last: "Lovelace",
+  //   born: 1815,
+  // });
+});
+
+/*
 // Get firebase config info
 const firebaseConfig = require("./config/firebase").firebaseConfig;
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
-app.post("/upload", upload.single("picture"), (req, res) => {
-  console.log(req.file);
-  res.send("Uploading file!");
+
+// Set up routes
+const images = require("./routes/image");
+
+app.use("images/", images);
+ 
+*/
+
+const express = require("express");
+const firebase = require("firebase");
+const cloudinary = require("cloudinary").v2;
+const bodyParser = require("body-parser");
+
+const app = express();
+// body parser configuration
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Get cloudinary keys
+const { cloudinary_api_key, cloudinary_api_secret } = require("./config/keys");
+// Cloudinary config
+cloudinary.config({
+  cloud_name: "dloimnf49",
+  api_key: cloudinary_api_key,
+  api_secret: cloudinary_api_secret,
 });
 
-app.get("/", async (req, res) => {
-  res.send("Hello World");
-  const docRef = db.collection("users").doc("alovelace");
+const { cloudinaryUpload } = require("./services/cloudinary");
 
-  await docRef.set({
-    first: "Ada1",
-    last: "Lovelace",
-    born: 1815,
-  });
+app.get("/", (request, response) => {
+  response.json({ message: "Hey! This is your server response!" });
+});
+
+// image upload API
+app.post("/upload", (request, response) => {
+  // collected image from a user
+  const data = {
+    image: request.body.image,
+  };
+
+  cloudinaryUpload(data.image)
+    .then((result) => {
+      console.log("Result is");
+      console.log(result.id);
+      console.log(result.url);
+      response.status(200).send({
+        result,
+      });
+    })
+    .catch((err) => {
+      response.status(500).send({
+        message: "failure",
+        error,
+      });
+    });
 });
 
 // Server running on port 5000
---
+++ services/cloudinary.js
@@ -0,0 +1,21 @@
+const cloudinary = require("cloudinary");
+
+// Helper methods to handle Cloudinary Upload, Download and Delete
+
+const cloudinaryUpload = (image) => {
+  return new Promise((resolve, reject) => {
+    // upload image here
+    cloudinary.uploader
+      .upload(image)
+      .then((result) => {
+        resolve({ id: result.public_id, url: result.secure_url });
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+};
+
+module.exports = {
+  cloudinaryUpload,
+};
