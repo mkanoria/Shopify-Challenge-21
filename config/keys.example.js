de249163cb58c737660d5d9bb068ecb6b9a21a1e
Mayank Kanoria <mayankkanoria@gmail.com>
2021-01-15 01:50:23 -0500

    Add sample config file

---
+++ config/keys.example.js
@@ -0,0 +1,13 @@
+module.exports = {
+  cloudinary_api_key: "YOUR KEY HERE",
+  cloudinary_api_secret: "YOUR SECRET HERE",
+  firebaseConfig: {
+    apiKey: "API KEY",
+    authDomain: "YOUR INFO",
+    projectId: "YOUR INFO",
+    storageBucket: "YOUR INFO",
+    messagingSenderId: "YOUR INFO",
+    appId: "YOUR INFO",
+    measurementId: "YOUR INFO",
+  },
+};
